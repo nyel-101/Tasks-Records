eed4f11c0733edbf047603d43992bd02f05e7285
nyel-101 <dandevjr@gmail.com>
2025-02-27 21:59:42 +0000

    test(config): fix logging interface

--- furry/tests/queries.js
+++ furry/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dfaef700ec6b4bdcab5e235f9ad05730 = () => {
-  return '2026-04-20T02:45:34Z';
+const node_baac85682d25478faeea6da82f91ed3d = () => {
+  return '2025-02-27T21:59:42Z';
 };
