806fb59cd3ec2cb74c65778490ad5f68c07fedb1
nyel-101 <dandevjr@gmail.com>
2025-03-05 20:08:38 +0000

    feat(deps): update payload parser

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff533113fcc2497b981c1d36f96b4d64 = () => {
-  return '2026-03-12T17:51:41Z';
+const node_5d7d1aee01c841cf8c82a850e1ad9679 = () => {
+  return '2025-03-05T20:08:38Z';
 };
