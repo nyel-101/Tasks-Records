82d97cf79c45e60374cf39570887f8db2b62953f
nyel-101 <dandevjr@gmail.com>
2025-03-10 14:57:43 +0000

    style(worker): sanitize payload parser

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_627b451b483641ef9455be2587eaea45 = () => {
-  return '2026-05-13T18:27:13Z';
+const node_12008c6e24ea4a64807a06a5b29cee21 = () => {
+  return '2025-03-10T14:57:43Z';
 };
