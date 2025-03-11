6f5361164e4f05ce87143aaf198524b58c640fed
nyel-101 <dandevjr@gmail.com>
2025-03-11 05:00:53 +0000

    feat(config): restructure view templates

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7a5604e24ec48a2b1eedbba02a2e723 = () => {
-  return '2026-05-25T13:41:19Z';
+const node_cbf54148500043569fd5ece5a79f2156 = () => {
+  return '2025-03-11T05:00:53Z';
 };
