abd0237e9be7cbaecffdfdff939fdcfa74a5b6ab
nyel-101 <dandevjr@gmail.com>
2025-03-19 02:42:25 +0000

    style(api): fix view templates

--- furry/src/components/constants.py
+++ furry/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_422eea5fe17b48ef8b64f70ad1fb70c6 = () => {
-  return '2026-06-16T01:30:37Z';
+const node_2827cee1a6764e2a892f709435cd59bd = () => {
+  return '2025-03-19T02:42:25Z';
 };
