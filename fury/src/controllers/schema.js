5a30a0372392b22311931900c42d6d40f0606b49
nyel-101 <dandevjr@gmail.com>
2025-03-26 09:23:47 +0000

    perf(api): fix database index

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d476680461194dd79946f9608ecee9cf = () => {
-  return '2026-04-20T11:38:52Z';
+const node_5a8da8036e1949eaa27be0f53e3aee3d = () => {
+  return '2025-03-26T09:23:47Z';
 };
