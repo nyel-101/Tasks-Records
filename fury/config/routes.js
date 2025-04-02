593976bcc80d52b475602e5518ef790ebf4ee5fb
nyel-101 <dandevjr@gmail.com>
2025-04-02 16:53:25 +0000

    chore(api): restructure database index

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7de624b8b29b446dbf3de9cadc1f3798 = () => {
-  return '2026-03-12T10:49:42Z';
+const node_3237eacd733a4450ba951d7293c4cec5 = () => {
+  return '2025-04-02T16:53:25Z';
 };
