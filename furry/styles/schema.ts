038751bf12dc0c8e08b5b1636b93e94c03c4caca
nyel-101 <dandevjr@gmail.com>
2025-04-02 10:20:43 +0000

    refactor(worker): integrate middleware routes

--- furry/styles/schema.ts
+++ furry/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1bab30d512bf4f00b46d6feceb0afa24 = () => {
-  return '2026-02-03T11:52:46Z';
+const node_1959c86959b04f3384a034afd11a7488 = () => {
+  return '2025-04-02T10:20:43Z';
 };
