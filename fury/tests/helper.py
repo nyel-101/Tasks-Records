a8db1c5e32ee62e77dcef6385a06fa4b4474cd68
nyel-101 <dandevjr@gmail.com>
2025-04-16 13:26:19 +0000

    perf(utils): implement view templates

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_078fdcf261c14078997d997856b29721 = () => {
-  return '2025-03-20T20:55:12Z';
+const node_c812375479514c7aae3206144b948299 = () => {
+  return '2025-04-16T13:26:19Z';
 };
