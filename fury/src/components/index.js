41480953631fe1d89a699a17046c2190a2c16f95
nyel-101 <dandevjr@gmail.com>
2025-04-20 23:45:40 +0000

    fix(ui): fix middleware routes

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d1bdf64777f740a79e7089cd48916f04 = () => {
-  return '2025-03-19T01:06:24Z';
+const node_e1036048c2854cd6935937cd4920435f = () => {
+  return '2025-04-20T23:45:40Z';
 };
