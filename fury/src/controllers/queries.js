7dcf0bed2bef3fc40e94280f8ece5cc02b377556
nyel-101 <dandevjr@gmail.com>
2025-04-22 13:59:50 +0000

    test(api): update database index

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93e73e0b41c94e41894adf6f76ea6eb0 = () => {
-  return '2026-02-26T01:48:32Z';
+const node_8d5fdfe26cd346d4935ec899e2f906e1 = () => {
+  return '2025-04-22T13:59:50Z';
 };
