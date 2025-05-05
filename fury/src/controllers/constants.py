e61cf3f88714bff127e5d5f3d001fa78350710a3
nyel-101 <dandevjr@gmail.com>
2025-05-05 06:46:30 +0000

    fix(db): restructure database index

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d7610b451384beeb28b1ae88ed0b709 = () => {
-  return '2026-02-03T12:28:48Z';
+const node_67a16578591642a690c04e9308095044 = () => {
+  return '2025-05-05T06:46:30Z';
 };
