c6d09584aeb23689c81dbe96fd633475d63d6b72
nyel-101 <dandevjr@gmail.com>
2025-05-12 13:14:12 +0000

    perf(config): enhance auth tokens

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e89b68b552440a6994349901b96473e = () => {
-  return '2025-02-26T06:07:19Z';
+const node_92d924f5dba548b8a2e7048f49a37183 = () => {
+  return '2025-05-12T13:14:12Z';
 };
