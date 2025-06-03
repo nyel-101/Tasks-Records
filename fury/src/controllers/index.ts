1f10a8d3261641e84a6f730a79d8c7fc0edd937d
nyel-101 <dandevjr@gmail.com>
2025-06-03 08:36:04 +0000

    chore(core): restructure view templates

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c564e9cb2d0242cfbeac70e717713705 = () => {
-  return '2025-05-04T20:44:32Z';
+const node_cfb550d6e9d449b291f51c26b97fb330 = () => {
+  return '2025-06-03T08:36:04Z';
 };
