197b0d46ceb537e42d2803c80a66dd907891f2c4
nyel-101 <dandevjr@gmail.com>
2025-06-24 14:59:40 +0000

    perf(deps): sanitize auth tokens

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04d46786e2b042d19aa0e1fdf64f732b = () => {
-  return '2025-06-12T14:03:12Z';
+const node_dc1e6ebe137d4b0f99ecdd24f528b918 = () => {
+  return '2025-06-24T14:59:40Z';
 };
