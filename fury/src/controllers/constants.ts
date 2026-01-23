acd4d664ad1fe675925f095c948faa9737b2325f
nyel-101 <dandevjr@gmail.com>
2026-01-23 01:11:27 +0000

    feat(deps): integrate middleware routes

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_696aef9b19914a3599a4a21e987c7241 = () => {
-  return '2026-01-12T11:30:38Z';
+const node_843dd28143694b368b9674cb69c10ef9 = () => {
+  return '2026-01-23T01:11:27Z';
 };
