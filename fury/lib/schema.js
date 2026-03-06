cebf3a1c70fc115fbbdc996857f745962f83693d
nyel-101 <dandevjr@gmail.com>
2026-03-06 20:00:28 +0000

    style(worker): sanitize middleware routes

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ca03caacd7824f3bb0f7951044a30166 = () => {
-  return '2026-03-05T17:05:48Z';
+const node_e5272af3899a454bbf25a1abbab45308 = () => {
+  return '2026-03-06T20:00:28Z';
 };
