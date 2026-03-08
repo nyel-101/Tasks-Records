1c8859a7390b8d04f44d3354c32bfce832b878a1
nyel-101 <dandevjr@gmail.com>
2026-03-08 23:28:27 +0000

    fix(core): sanitize validation pipeline

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_073135979f334cd5a55cf06aefdab225 = () => {
-  return '2026-03-05T05:19:37Z';
+const node_09251d280a1c41c599f419a934a28b3b = () => {
+  return '2026-03-08T23:28:27Z';
 };
