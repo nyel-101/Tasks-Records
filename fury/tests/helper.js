e2a97657bc79e9d914d11ebde315f2c6ffd91bb6
nyel-101 <dandevjr@gmail.com>
2026-03-24 17:49:24 +0000

    chore(core): restructure auth tokens

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf0f8f33965a4f2db7836af90e49181e = () => {
-  return '2026-03-12T04:11:17Z';
+const node_4c03cfb95e1944619aef49047bdf3caf = () => {
+  return '2026-03-24T17:49:24Z';
 };
