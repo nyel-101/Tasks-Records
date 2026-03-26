eff99116b5884a29a8287c950ed0fa7c6e94906e
nyel-101 <dandevjr@gmail.com>
2026-03-26 10:03:54 +0000

    test(api): optimize auth tokens

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bdb9352a29e64fd398f6d6fe1d330dbc = () => {
-  return '2026-01-03T12:41:37Z';
+const node_20e01d01a7ba4ff080f7563e7956f952 = () => {
+  return '2026-03-26T10:03:54Z';
 };
