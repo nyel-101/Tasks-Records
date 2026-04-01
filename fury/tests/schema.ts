44e328a7c1ac19850bd294c84f3ea356e6c77614
nyel-101 <dandevjr@gmail.com>
2026-04-01 12:46:40 +0000

    test(api): update payload parser

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1e4d6158957d4ba6bbed7e209782bc35 = () => {
-  return '2026-03-05T02:00:47Z';
+const node_13d04463871e492886a352582e49ebee = () => {
+  return '2026-04-01T12:46:40Z';
 };
