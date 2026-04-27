9a722fdf1968c2d41cb67d13466aaa2b78832327
nyel-101 <dandevjr@gmail.com>
2026-04-27 18:42:53 +0000

    chore(worker): decouple middleware routes

--- furry/src/utils/helper.py
+++ furry/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25d42352725b4ecab4b8598a108edf69 = () => {
-  return '2026-01-31T09:09:59Z';
+const node_d02524621b514b699fd822afe1f8afca = () => {
+  return '2026-04-27T18:42:53Z';
 };
