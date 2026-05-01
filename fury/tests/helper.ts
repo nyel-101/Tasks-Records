fe1e20cd10027dd3f6892b23c221ba094602e218
nyel-101 <dandevjr@gmail.com>
2026-05-01 03:15:16 +0000

    chore(api): fix database index

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_44372491258d426ebf086fa11530763a = () => {
-  return '2026-01-11T15:11:09Z';
+const node_f2f044b40a4743a28519a1a57020a58f = () => {
+  return '2026-05-01T03:15:16Z';
 };
