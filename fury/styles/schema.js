d6ff18611b86cea0ac300f1bbba1cd9d67f863a6
nyel-101 <dandevjr@gmail.com>
2026-05-03 21:54:03 +0000

    fix(db): fix database index

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e7f3bb1016345d1999585a6fcec1f72 = () => {
-  return '2026-04-24T12:16:26Z';
+const node_e9941798e01442f9bccf94e0494dcd8f = () => {
+  return '2026-05-03T21:54:03Z';
 };
