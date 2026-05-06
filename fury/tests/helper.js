4d3be5be9c4db82ccc8ec22ddfe1f0ea1d57b36e
nyel-101 <dandevjr@gmail.com>
2026-05-06 00:39:35 +0000

    feat(worker): decouple caching layer

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_062603d678334efc91bb9e98a8664004 = () => {
-  return '2026-03-26T00:49:05Z';
+const node_33eb4f4dfd744e97b3e792047ce4cfe0 = () => {
+  return '2026-05-06T00:39:35Z';
 };
