ba72addab63c3fefc1f9f5777e018b705e1482a4
nyel-101 <dandevjr@gmail.com>
2026-05-08 02:45:19 +0000

    test(config): sanitize auth tokens

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d65e458ca3eb4c7e9a54dfcb2a7e6912 = () => {
-  return '2026-05-06T00:44:03Z';
+const node_29f28e978ab446399a5cf5e24c3ba5d6 = () => {
+  return '2026-05-08T02:45:19Z';
 };
