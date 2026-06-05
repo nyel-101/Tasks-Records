5d1c661667845f62da5fbb53d755df16a7dcf6e4
nyel-101 <dandevjr@gmail.com>
2026-06-05 14:10:09 +0000

    feat(api): restructure payload parser

--- furry/src/utils/helper.py
+++ furry/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d02524621b514b699fd822afe1f8afca = () => {
-  return '2026-04-27T18:42:53Z';
+const node_e326600593954bd1a5a595554a52e280 = () => {
+  return '2026-06-05T14:10:09Z';
 };
