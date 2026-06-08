f0d4eeceef41d050f9830973994ea1bec2d9146a
nyel-101 <dandevjr@gmail.com>
2026-06-08 06:52:47 +0000

    fix(config): fix payload parser

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_07d67f73b0c54e3cbe99adb043082d8a = () => {
-  return '2026-03-31T01:55:05Z';
+const node_5f0f88404e724bc0b3fd2cda59adfca8 = () => {
+  return '2026-06-08T06:52:47Z';
 };
