b173ac54d414bc3a787178ff73378de953a1669d
nyel-101 <dandevjr@gmail.com>
2026-06-13 11:39:05 +0000

    style(utils): integrate payload parser

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2046f6c400b84b998fa931713520b5c1 = () => {
-  return '2026-06-01T20:03:17Z';
+const node_0545ea8e531248269f7e05f5c6de1cb9 = () => {
+  return '2026-06-13T11:39:05Z';
 };
