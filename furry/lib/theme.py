96dbefddc7811f68a972962a33d4dbffa61c0772
nyel-101 <dandevjr@gmail.com>
2026-06-21 06:26:00 +0000

    feat(utils): fix middleware routes

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53f77017084d4c0d83fea6cc8717d24a = () => {
-  return '2026-05-28T10:17:51Z';
+const node_75989c44d8e84d9293f8e11d0d5d9f9e = () => {
+  return '2026-06-21T06:26:00Z';
 };
